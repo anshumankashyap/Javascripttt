843df7d91953cbf0f266da7fd79c3789978e1a51
Ansuman Panda <124714817+anshumankashyap@users.noreply.github.com>
2025-03-11 15:34:18 +0000

    Added operation

---
+++ 01_basic/03_conversion_operation.js
@@ -0,0 +1,42 @@
+//let score = "33abc"
+//
+//console.log(typeof score);
+//console.log(typeof score);
+//
+//let valueInNumber = Number (score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+//
+////33=>33
+////33abc=>NaN
+////true=>1; false=0;
+//
+//let isLoggedIn = 1
+//
+//let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+//
+////1=>True; 0=>False
+//// ""=> False
+////"Ansuman" (any string) => True
+//
+//let someNumber = 33
+//let stringNumber = String(someNumber)
+//
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+//
+///**********************************Operation*************************/
+//let value = 21
+//let newScore = -value
+//
+//console.log(newScore);
+//
+//let str1 = "Hello"
+//let str2 = " Anshuman"
+//let str3 = str1 + str2
+//console.log(str3);
+
+let gameCoounter = 100
+gameCoounter++
+console.log(gameCoounter);
