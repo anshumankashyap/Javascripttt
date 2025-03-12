7c6351cf2895c725f98f45ef96143488b8655a76
Ansuman Panda <124714817+anshumankashyap@users.noreply.github.com>
2025-03-12 11:00:38 +0000

    comparision

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,15 @@
+console.log(1>2);
+console.log("1">2);
+
+console.log(null == 0);
+console.log(null > 0);
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null <= 0);
+
+console.log(undefined == 2);
+console.log(undefined >= 2);
+console.log(undefined <= 2);
+
+// ===
+console.log("2"===2);
