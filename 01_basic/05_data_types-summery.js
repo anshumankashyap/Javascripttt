a212c04697203137af2109b5b056ce6c43b7d5ab
Ansuman Panda <124714817+anshumankashyap@users.noreply.github.com>
2025-03-12 11:43:15 +0000

    sunmmery

---
+++ 01_basic/05_data_types-summery.js
@@ -0,0 +1,21 @@
+//premitive
+// 7 types -> sstring, NUmber, Boolean, Null, Undefined, Symbol, BigInt
+
+let score = 100
+let scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail; // let userEmail = undefined //undefined can be declared using both format  
+
+const id = Symbol('12345')
+const anotherId = Symbol('12345')
+
+console.log(id == anotherId);
+
+
+// Reference (Non Premitive)
+// Array, Objects, Functions
+
+// Javascript is a dynamic type language
+
