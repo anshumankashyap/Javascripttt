2d43074449cd6cea85726b35a7253e453d34baba
Ansuman Panda <124714817+anshumankashyap@users.noreply.github.com>
2025-03-19 21:17:15 +0000

    Sttring

--- 01_basic/05_string.js
+++ 01_basic/05_string.js
@@ -6,10 +6,17 @@ console.log(name + repoCount); // Avoid using this syntax to print a string
 console.log(`Hello my name is ${name} & my repocount is ${repoCount}`); /* This is the better way to print a string
 this is also called as string interpolation */
 
-const gameName =  new String("Valo")
+const gameName =  new String("Valorant")
 
 console.log(gameName[2]); //Access the keys
-
 console.log(gameName.charAt(1)); //Print the characters present in thaat position
-
 console.log(gameName.indexOf('V')); //Print the position of the character present
+
+const anotherGame = gameName.substring(0, 4)
+console.log(anotherGame);
+
+const anotherName = gameName.slice(-8, 4)
+console.log(anotherName);
+
+const anotherSttring = ("         Anshuman    ")
+console.log(anotherSttring.trim());
