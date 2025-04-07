5142cc79e2ec12b80b28781091c9236a22c2acf9
Ansuman Panda <124714817+anshumankashyap@users.noreply.github.com>
2025-04-07 11:17:53 +0000

    Number & Maths

---
+++ 01_basic/06_nums_maths.js
@@ -0,0 +1,23 @@
+//----------------------------NUMBER-----------------------
+const score = 100
+console.log(score);
+
+const Balance = new Number (444)
+console.log(Balance);
+
+console.log(Balance.toString().length);
+console.log(Balance.toFixed(2));
+
+const anotherNumber = 23.556
+console.log(anotherNumber.toPrecision(3));
+
+const someNumbers = 100000000
+console.log(someNumbers.toLocaleString('en-IN'));
+
+
+//----------------------------------MATHS-----------------------
+console.log(Math);
+console.log(Math.abs(-7));
+console.log(Math.round(4.5)); //It round off the value
+console.log(Math.ceil(4.2));//It round off to the upper value like 4.2 to 5
+console.log(Math.floor(4.9));//It round off to the lower value like 4.9 to 4
