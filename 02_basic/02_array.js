a832b1c3a5ab174abf99eb9f643ac5d3a655c0ae
Ansuman Panda <124714817+anshumankashyap@users.noreply.github.com>
2025-04-21 16:48:58 +0000

    Learnewd to concat, spread operator, & Array function

--- 02_basic/02_array.js
+++ 02_basic/02_array.js
@@ -1,5 +1,31 @@
-const marvel_heros = ["Spiderman", "Ironman", "Thor"]
-const DC_heros = ["Superman", "Flash", "Batman"]
+//const marvel_heros = ["Spiderman", "Ironman", "Thor"]
+//const DC_heros = ["Superman", "Flash", "Batman"]
+//
+////marvel_heros.push(DC_heros)
+////console.log(marvel_heros);
+//
+////To aavoid the above situation we can use following situation
+//
+//const allHeros = marvel_heros.concat(DC_heros)
+//console.log(allHeros);
+//
+///*This is a spread operator*/
+//const allNewHeros = [...marvel_heros, ...DC_heros]
+//console.log(allNewHeros);
+//
+//const anotherArray = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+//console.log(anotherArray);
+//
+//const realAnotherArray = anotherArray.flat(Infinity)
+//console.log(realAnotherArray);
 
-marvel_heros.push(DC_heros)
-console.log(marvel_heros);
+console.log(Array.isArray("Anshuman"));
+console.log(Array.from("Anshuman"));
+console.log(Array.from({name: "Anshuman"}));//intresting (Here we have to define a specific key(name:) or values(Anshuman) to make an array)
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
